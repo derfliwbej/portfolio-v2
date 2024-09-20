15ec0d587204bfae2878855e87158e63691b8ba4
Jeb Wilfred Panganiban <jwdpanganiban@gmail.com>
2024-09-20 11:27:56 +0800

    Initial page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Header from './components/typography/Header';
+
 export default function Home() {
-  return <main className='h-screen w-screen bg-dark-1 text-fg'>Home</main>;
+  return (
+    <main className='h-full w-full bg-dark-2 text-fg p-7'>
+      <div>
+        <Header size='h2'>Jeb Wilfred Panganiban</Header>
+        <Header size='h4'>Software Engineer</Header>
+      </div>
+      <div>
+        <Header size='h5'>About</Header>
+      </div>
+    </main>
+  );
 }
